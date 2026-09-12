f9fee4dccf6c202368d6d3f6e1db248d69728f50
HarishkannaR11 <harishkannar2312050@ssn.edu.in>
2026-09-12 23:02:50 +0530

    Frontend: Remove remaining dashboard components and routing, keeping only AgentConsole

--- ops-dashboard/src/main.tsx
+++ ops-dashboard/src/main.tsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AppLayout from './AppLayout'
-import RunsList from './pages/RunsList'
-import RunDetail from './pages/RunDetail'
+import AgentConsole from './pages/AgentConsole'
 import './index.css'
-
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<RunsList />} />
-            <Route path="runs/:runId" element={<RunDetail />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <AgentConsole />
   </React.StrictMode>
 )
